e557daa2c1005daf5948b80311382dcc2d4bf73f
DjDeveloperr <djsandhu2005@gmail.com>
2021-12-03 16:31:28 +0530

    day 3

---
+++ 3/solution.ts
@@ -0,0 +1,67 @@
+// https://adventofcode.com/2021/day/3
+
+export function solve(numbers: string[]) {
+  const commonLength = numbers[0].length;
+  let gammaRate = 0;
+  let epsilonRate = 0;
+
+  for (let i = 0; i < commonLength; i++) {
+    let ones = 0, zeroes = 0;
+    for (const num of numbers) {
+      const bit = num[commonLength - i - 1];
+      if (bit === "0") ones++;
+      else zeroes++;
+    }
+
+    const mcb = ones > zeroes ? 0 : 1;
+    const lcb = mcb ^ 1;
+
+    gammaRate += mcb * 2 ** i;
+    epsilonRate += lcb * 2 ** i;
+  }
+
+  return gammaRate * epsilonRate;
+}
+
+function step(
+  rating: 0 | 1,
+  index: number,
+  f: string[],
+): [string[], number | undefined] {
+  let ones = 0, zeroes = 0;
+
+  for (let j = 0; j < f.length; j++) {
+    const bit = f[j][index];
+    if (bit === "0") ones++;
+    else zeroes++;
+  }
+
+  const bit = String(
+    ones === zeroes ? (rating ^ 1) : ((ones > zeroes ? 0 : 1) ^ rating),
+  );
+
+  f = f.filter((e) => e[index] === bit);
+  if (f.length === 1) {
+    return [[], parseInt(f[0], 2)];
+  } else return [f, undefined];
+}
+
+export function solvePartTwo(numbers: string[]) {
+  const commonLength = numbers[0].length;
+  let r1: number | undefined = undefined,
+    r2: number | undefined = undefined,
+    r1Filter: string[] = [...numbers],
+    r2Filter: string[] = [...numbers];
+
+  for (let i = 0; i < commonLength; i++) {
+    if (typeof r1 === "undefined") {
+      [r1Filter, r1] = step(0, i, r1Filter);
+    }
+
+    if (typeof r2 === "undefined") {
+      [r2Filter, r2] = step(1, i, r2Filter);
+    }
+  }
+
+  return r1! * r2!;
+}
---
+++ 3/test.ts
@@ -0,0 +1,45 @@
+import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import { solve, solvePartTwo } from "./solution.ts";
+
+const EXAMPLE_CASE = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010",
+];
+
+const INPUT_CASE = Deno.readTextFileSync(
+  new URL("./input.txt", import.meta.url),
+).replaceAll("\r\n", "\n").split("\n");
+
+Deno.test("part one", async (t) => {
+  await t.step("example case", () => {
+    const result = solve(EXAMPLE_CASE);
+    assertEquals(result, 198);
+  });
+
+  await t.step("input case", () => {
+    const result = solve(INPUT_CASE);
+    assertEquals(result, 4174964);
+  });
+});
+
+Deno.test("part two", async (t) => {
+  await t.step("example case", () => {
+    const result = solvePartTwo(EXAMPLE_CASE);
+    assertEquals(result, 230);
+  });
+
+  await t.step("input case", () => {
+    const result = solvePartTwo(INPUT_CASE);
+    assertEquals(result, 4474944);
+  });
+});
